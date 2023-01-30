f4defbe2273edf2ebb0d10af02af56d092d6522c
gr413_buayur <bushueva.03@inbox.ru>
2023-01-30 10:21:45 +0700

    Выполнено 3 задание и исправлены некоторые недочеты в коде

--- js/main.js
+++ js/main.js
@@ -1,4 +1,3 @@
-//1
 let app = new Vue({
     el: '#app',
     data: {
@@ -6,14 +5,10 @@ let app = new Vue({
         description: "A pair of warm, fuzzy socks",
         image: "./assets/vmSocks-blue-onWhite.jpg",
         altText: "A pair of socks",
-        link: "https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks"
+        link: "https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks",
+        inStock: true,
+        inventory: 100,
+        onSale: true,
     }
  }) 
 
- //2
- let link = new Vue({
-    el:"#link",
-    data: {
-
-    }
- })
