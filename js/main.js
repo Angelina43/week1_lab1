b9e2963dba851c7aaffda639e16669637149cd44
angelina43 <bushueva.03@inbox.ru>
2023-01-31 09:25:45 +0700

    Выполнено 8 задание

--- js/main.js
+++ js/main.js
@@ -1,40 +1,90 @@
-let app = new Vue({
-    el: '#app',
-    data: {
-        product: "Socks",
-        brand: 'Vue Mastery',
-        description: "A pair of warm, fuzzy socks",
-        selectedVariant: 0,
-        altText: "A pair of socks",
-        link: "https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks",
-        inventory: 100,
-        details: ['80% cotton', '20% polyester', 'Gender-neutral'],
-        variants: [
-            {
-                variantId: 2234,
-                variantColor: 'green',
-                variantImage: "./assets/vmSocks-green-onWhite.jpg",
-                variantQuantity: 10,
-                variantSale: "not on Sale"
-            },
-            {
-                variantId: 2235,
-                variantColor: 'blue',
-                variantImage: "./assets/vmSocks-blue-onWhite.jpg",
-                variantQuantity: 0,
-                variantSale: "on Sale"
-            }
-        ],
-        sizes: ['S', 'M', 'L', 'XL', 'XXL', 'XXXL'],
-        cart: 0,
-        onSale: "On sale",
+Vue.component('product-details', {
+    props: {
+        details: {
+            type: Array,
+            required: true
+        }
+    },
+    template: `
+    <ul>
+      <li v-for="detail in details">{{ detail }}</li>
+    </ul>`
+})
+
+Vue.component('product', {
+    template: `
+   <div class="product">
+	<div class="product-image">
+                <img :src="image" :alt="altText"/>
+            </div>
+            <div class="product-info">
+                <h1>{{ title }}</h1>
+             <span v-if="onSale">{{ sale }}</span>
+                <p>{{ description }}</p>
+                <p v-if="inStock">In stock</p>
+                <p v-else :class="{ outOfStock: !inStock }">Out of stock</p>
+                <product-details :details="details"></product-details>
+                <div class="color-box"
+                     v-for="(variant, index) in variants"
+                     :key="variant.variantId"
+                     :style="{ backgroundColor:variant.variantColor }"
+                     @mouseover="updateProduct(index)">
+                </div>
+
+                
+                <p>Shipping: {{ shipping }}</p>
+                <div class="cart">
+                    <p>Cart({{ cart }})</p>
+                </div>
+                <button v-on:click="addToCart" :disabled="!inStock" :class="{ disabledButton: !inStock }">Add to cart</button>
+                <button v-on:click="deleteCart">Delete cart</button>
+            </div>
+            <a :href="link">More products like this</a>
+   </div>
+ `,
+    props: {
+        premium: {
+            type: Boolean,
+            required: true
+        }
+    },
+    data() {
+        return {
+            product: "Socks",
+            brand: 'Vue Mastery',
+            description: "A pair of warm, fuzzy socks",
+            selectedVariant: 0,
+            altText: "A pair of socks",
+            link: "https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks",
+            inventory: 100,
+            details: ['80% cotton', '20% polyester', 'Gender-neutral'],
+            variants: [
+                {
+                    variantId: 2234,
+                    variantColor: 'green',
+                    variantImage: "./assets/vmSocks-green-onWhite.jpg",
+                    variantQuantity: 10,
+                    variantSale: "not on Sale"
+                },
+                {
+                    variantId: 2235,
+                    variantColor: 'blue',
+                    variantImage: "./assets/vmSocks-blue-onWhite.jpg",
+                    variantQuantity: 0,
+                    variantSale: "on Sale"
+                }
+            ],
+            sizes: ['S', 'M', 'L', 'XL', 'XXL', 'XXXL'],
+            cart: 0,
+            onSale: "On sale",
+        }
     },
     methods: {
         addToCart() {
             this.cart += 1
         },
-        deleteCart(){
-            if (this.cart > 0){
+        deleteCart() {
+            if (this.cart > 0) {
                 this.cart -= 1
             }
         },
@@ -42,20 +92,32 @@ let app = new Vue({
             this.selectedVariant = index;
         },
     },
-    //вычисляемые свойства
     computed: {
-        title(){
+        title() {
             return this.brand + ' ' + this.product;
         },
         image() {
             return this.variants[this.selectedVariant].variantImage;
         },
-        inStock(){
+        inStock() {
             return this.variants[this.selectedVariant].variantQuantity
         },
-        sale(){
+        sale() {
             return this.brand + ' ' + this.product + ' ' + this.variants[this.selectedVariant].variantSale;
-        }
-    },
+        },
+        shipping() {
+            if (this.premium) {
+                return "Free";
+            } else {
+                return 2.99
+            }
+        },
+    }
 })
 
+let app = new Vue({
+    el: '#app',
+    data: {
+        premium: true
+    }
+})
